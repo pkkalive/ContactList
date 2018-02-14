ee9cb522d2dbd1ce3aafe38a2843e47aca1ebb15
Purushotham Kumar Kalive <Tammu@PurushothamsMBP.local.lan>
2018-02-14 09:13:25 +0530

    added create contact functionality

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -7,7 +7,6 @@ class CreateContact extends Component {
   handleSubmit = (e) => {
     e.preventDefault()
     const values = serializeForm(e.target, {hash : true})
-    console.log(values);
     if (this.props.onCreateContact)
       this.props.onCreateContact(values)
 
